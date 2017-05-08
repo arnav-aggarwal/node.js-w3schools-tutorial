a6d993bb5bf4aa0400b5c5071c61cc3f2610b748
Arnav <arnav.aggarwal@thecontrolgroup.com>
2017-05-08 10:28:36 -0700

    Begin using fs module

--- myfirst.js
+++ myfirst.js
@@ -1,10 +1,28 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    //write head with status code & response headers
-    res.writeHead(200, { 'Content-Type': 'text/html'});
-    res.write(req.url);
-    res.end();
+    fs.readFile('demofile1.html', (err, data) => {
+        //write head with status code & response headers
+        res.writeHead(200, { 'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    });
+});
+
+fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+});
+
+fs.open('mynewfile2.txt', 'w', function (err, file) {
+  if (err) throw err;
+  console.log('Saved!');
+});
+
+fs.unlink('mynewfile2.txt', function (err) {
+  if (err) throw err;
+  console.log('File deleted!');
 });
 
 server.listen(8081);
