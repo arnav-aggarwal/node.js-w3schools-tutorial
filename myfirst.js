ad48f25be67c2615536191b254fbea964a195290
Arnav <arnav.aggarwal@thecontrolgroup.com>
2017-05-10 16:05:15 -0700

    Set up basic routing

--- myfirst.js
+++ myfirst.js
@@ -1,28 +1,21 @@
 const http = require('http');
+const url = require('url');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    fs.readFile('demofile1.html', (err, data) => {
-        //write head with status code & response headers
-        res.writeHead(200, { 'Content-Type': 'text/html'});
-        res.write(data);
-        res.end();
+  fs.readFile('demofile1.html', (err, data) => {
+    const filename = '.' + url.parse(req.url).pathname;
+    fs.readFile(filename, (err, data) => {
+      if(err) {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        return res.end('404 Not Found');
+      }
+
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
     });
+  });
 });
 
-fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
-  if (err) throw err;
-  console.log('Saved!');
-});
-
-fs.open('mynewfile2.txt', 'w', function (err, file) {
-  if (err) throw err;
-  console.log('Saved!');
-});
-
-fs.unlink('mynewfile2.txt', function (err) {
-  if (err) throw err;
-  console.log('File deleted!');
-});
-
-server.listen(8081);
+server.listen(8081, () => console.log('Listening on 8081'));
