0ef46b2dce235777df95b25841b6abbd320248a9
Arnav <arnav.aggarwal@thecontrolgroup.com>
2017-05-11 22:00:27 -0700

    Reorganize file structure and server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
 const formidable = require('formidable');
 
 const server = http.createServer((req, res) => {
@@ -13,13 +14,23 @@ const server = http.createServer((req, res) => {
         res.end();
       });
     });
-  } else {
+  } else if(req.url === '/') {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
     res.write('<input type="file" name="filetoupload"><br>');
     res.write('<input type="submit">');
     res.write('</form>');
     return res.end();
+  } else {
+    const file = '.' + url.parse(req.url).pathname;
+    fs.readFile('assets/' + file, (err, data) => {
+      if(err) {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        return res.end('404 not found');
+      }
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    });
   }
 });
 
