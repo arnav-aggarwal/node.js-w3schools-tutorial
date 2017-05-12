654450b189f421ecb84d37757630286cd108b9db
Arnav <arnav.aggarwal@thecontrolgroup.com>
2017-05-11 21:07:16 -0700

    Run npm init and get file upload working

--- myfirst.js
+++ myfirst.js
@@ -1,21 +1,28 @@
 const http = require('http');
-const url = require('url');
 const fs = require('fs');
+const formidable = require('formidable');
 
 const server = http.createServer((req, res) => {
-  fs.readFile('demofile1.html', (err, data) => {
-    const filename = '.' + url.parse(req.url).pathname;
-    fs.readFile(filename, (err, data) => {
-      if(err) {
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        return res.end('404 Not Found');
-      }
-
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      return res.end();
+  if(req.url === '/fileupload') {
+    const form = new formidable.IncomingForm();
+    form.parse(req, (err, fields, files) => {
+      const { path: filePath, name: fileName } = files.filetoupload;
+      fs.rename(filePath, `${__dirname}/${fileName}`, err => {
+        if(err) throw err;
+        res.write('File uploaded and moved!');
+        res.end();
+      });
     });
-  });
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+  }
 });
 
 server.listen(8081, () => console.log('Listening on 8081'));
+
+// https://www.w3schools.com/nodejs/nodejs_uploadfiles.asp
